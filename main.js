be0bd37fbb36e2f9180c3ddf6fa8200ce3535a95
thejordanprice <thejordanprice@gmx.com>
2018-02-06 22:54:37 -0500

    added an impression counter stat show in window.

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 300, height: 200})
+  mainWindow = new BrowserWindow({width: 300, height: 300})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
